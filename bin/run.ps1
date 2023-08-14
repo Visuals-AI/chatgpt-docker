34187db8328e215066262b610296c1276c9f4aba
EXP <289065406@qq.com>
2023-08-14 22:45:27 +0800

    更新启动脚本

--- bin/run.ps1
+++ bin/run.ps1
@@ -1,73 +1,91 @@
 # Powershell
 #------------------------------------------------
 # 运行 docker 服务（由于需要实时解析局域网 IP，需要管理员权限执行）
-# sudo bin/run.ps1
+# bin/run.ps1
 #           [-p ${PROTOCOL}]            # 页面使用 http/https 协议（默认 http）
-#           [-d ${DOMAIN}]              # 服务域名
+#           [-d ${DOMAIN}]              # 对公服务域名（浏览器访问地址）
 #           [-i ${IP}]                  # 服务器 IP（默认通过网卡取内网 IP，如果需要公网访问，需设置为公网 IP）
+#           [-k ${OPENAI_API_KEY}]      # ChatGPT API key
+#           [-m ${OPENAI_MODEL}]        # ChatGPT Model: gpt-4, gpt-4-0314, gpt-4-0613, gpt-4-32k, gpt-4-32k-0314, gpt-4-32k-0613, gpt-3.5-turbo-16k, gpt-3.5-turbo-16k-0613, gpt-3.5-turbo, gpt-3.5-turbo-0301, gpt-3.5-turbo-0613, text-davinci-003, text-davinci-002, code-davinci-002
+#           [-s ${SOCKS_PROXY_HOST}]    # Socks5 代理服务，和 HTTP 二选一，格式形如 host.docker.internal
+#           [-r ${SOCKS_PROXY_PORT}]    # Socks5 代理服务端口
+#           [-h ${HTTPS_PROXY}]         # HTTP 代理服务，和 Socks5 二选一，格式形如 http://host.docker.internal:10088
+#------------------------------------------------
+# 注： host.docker.internal 是 docker 内访问宿主机上的服务的固定地址
 #------------------------------------------------
 
 param(
-    [string]$Protocol = "http",
-    [string]$Domain = "local.chatgpt.com",
-    [string]$IP = ""
+    [string]$k = "", 
+    [string]$m = "gpt-3.5-turbo", 
+    [string]$p = "http",
+    [string]$d = "local.chatgpt.com",
+    [string]$i = "", 
+    [string]$s = "host.docker.internal", 
+    [string]$r = "0", 
+    [string]$h = ""
 )
 
+$OPENAI_API_KEY = $k
+$OPENAI_MODEL = $m
+$PROTOCOL = $p
+$DOMAIN = $d
+$INTER_IP = $i
+$SOCKS_PROXY_HOST = $s
+$SOCKS_PROXY_PORT = $r
+$HTTPS_PROXY = $h
 
-if (-not $IP) {
+
+if (-not $INTER_IP) {
   $interfaces = @("Wi-Fi", "Ethernet", "以太网")
   foreach ($int in $interfaces) {
     if ($output = (ipconfig $int 2>$null | Select-String -Pattern 'IPv4 Address')) {
-      $IP = $output -replace '.*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*', '$1'
+      $INTER_IP = $output -replace '.*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*', '$1'
       break
     }
   }
 
-  if (-not $IP) {
-    $IP = "127.0.0.1"
+  if (-not $INTER_IP) {
+    $INTER_IP = "127.0.0.1"
   }
 }
 
 function Set-Dns {
   param(
-    [string]$Domain,
-    [string]$InterIP
+    [string]$DOMAIN,
+    [string]$INTER_IP
   )
 
   $dns_file = "C:\Windows\System32\drivers\etc\hosts"
-  if (Select-String -Path $dns_file -Pattern $Domain) {
-    $from_reg = "^[0-9.]* ${Domain}$"
-    $to_str = "${InterIP} ${Domain}"
+  if (Select-String -Path $dns_file -Pattern $DOMAIN) {
+    $from_reg = "^[0-9.]* ${DOMAIN}$"
+    $to_str = "${INTER_IP} ${DOMAIN}"
     (Get-Content -Path $dns_file) -replace $from_reg, $to_str | Set-Content -Path $dns_file
     if (-not $?) {
       Write-Host "In order to update the inter IP in local hosts, please use 'Run as administrator' ..."
     }
   } else {
-    Add-Content -Path $dns_file -Value "$InterIP $Domain"
+    Add-Content -Path $dns_file -Value "$INTER_IP $DOMAIN"
   }
 }
 
 function Set-Env {
-  param(
-    [string]$Domain,
-    [string]$InterIP,
-    [string]$Protocol
-  )
-
   $env_file = ".env"
-  "DOMAIN=$Domain" | Out-File -Encoding utf8 -FilePath $env_file
-  "INTER_IP=$InterIP" | Out-File -Encoding utf8 -Append -FilePath $env_file
-  "PROTOCOL=$Protocol" | Out-File -Encoding utf8 -Append -FilePath $env_file
-  Write-Host "$InterIP $Domain"
+  "DOMAIN=$DOMAIN" | Out-File -Encoding utf8 -FilePath $env_file
+  "INTER_IP=$INTER_IP" | Out-File -Encoding utf8 -Append -FilePath $env_file
+  "PROTOCOL=$PROTOCOL" | Out-File -Encoding utf8 -Append -FilePath $env_file
+  "OPENAI_API_KEY=$OPENAI_API_KEY" | Out-File -Encoding utf8 -Append -FilePath $env_file
+  "OPENAI_MODEL=$OPENAI_MODEL" | Out-File -Encoding utf8 -Append -FilePath $env_file
+  "SOCKS_PROXY_HOST=$SOCKS_PROXY_HOST" | Out-File -Encoding utf8 -Append -FilePath $env_file
+  "SOCKS_PROXY_PORT=$SOCKS_PROXY_PORT" | Out-File -Encoding utf8 -Append -FilePath $env_file
+  "HTTPS_PROXY=$HTTPS_PROXY" | Out-File -Encoding utf8 -Append -FilePath $env_file
+  Write-Host "$INTER_IP $DOMAIN"
 }
 
-# FIXME
-# Set-Dns -Domain $Domain -InterIP $IP
-# Set-Env -Domain $Domain -InterIP $IP -Protocol $Protocol
 
+Set-Dns -DOMAIN $DOMAIN -INTER_IP $INTER_IP
+Set-Env
 
 
 docker-compose up -d
-
 docker ps
-Write-Host "Docker is running: ${Protocol}://${Domain}"
+Write-Host "Docker is running: ${PROTOCOL}://${DOMAIN}"
--- bin/run.sh
+++ bin/run.sh
@@ -3,16 +3,28 @@
 # 运行 docker 服务（由于需要实时解析局域网 IP，需要 sudo 权限执行）
 # sudo bin/run.sh
 #           [-p ${PROTOCOL}]            # 页面使用 http/https 协议（默认 http）
-#           [-d ${DOMAIN}]              # 服务域名
+#           [-d ${DOMAIN}]              # 对公服务域名（浏览器访问地址）
 #           [-i ${IP}]                  # 服务器 IP（默认通过网卡取内网 IP，如果需要公网访问，需设置为公网 IP）
+#           [-k ${OPENAI_API_KEY}]      # ChatGPT API key
+#           [-m ${OPENAI_MODEL}]        # ChatGPT Model: gpt-4, gpt-4-0314, gpt-4-0613, gpt-4-32k, gpt-4-32k-0314, gpt-4-32k-0613, gpt-3.5-turbo-16k, gpt-3.5-turbo-16k-0613, gpt-3.5-turbo, gpt-3.5-turbo-0301, gpt-3.5-turbo-0613, text-davinci-003, text-davinci-002, code-davinci-002
+#           [-s ${SOCKS_PROXY_HOST}]    # Socks5 代理服务，和 HTTP 二选一，格式形如 host.docker.internal
+#           [-r ${SOCKS_PROXY_PORT}]    # Socks5 代理服务端口
+#           [-h ${HTTPS_PROXY}]         # HTTP 代理服务，和 Socks5 二选一，格式形如 http://host.docker.internal:10088
+#------------------------------------------------
+# 注： host.docker.internal 是 docker 内访问宿主机上的服务的固定地址
 #------------------------------------------------
 
+OPENAI_API_KEY=""
+OPENAI_MODEL="gpt-3.5-turbo"
 PROTOCOL="http"
 DOMAIN="local.chatgpt.com"
 INTER_IP=""
+SOCKS_PROXY_HOST="host.docker.internal"
+SOCKS_PROXY_PORT="0"
+HTTPS_PROXY=""
 
 
-set -- `getopt p:d:i: "$@"`
+set -- `getopt p:d:i:k:m:s:r:h: "$@"`
 while [ -n "$1" ]
 do
   case "$1" in
@@ -21,6 +33,16 @@ do
     -d) DOMAIN="$2"
         shift ;;
     -i) INTER_IP="$2"
+        shift ;;
+    -k) OPENAI_API_KEY="$2"
+        shift ;;
+    -m) OPENAI_MODEL="$2"
+        shift ;;
+    -s) SOCKS_PROXY_HOST="$2"
+        shift ;;
+    -r) SOCKS_PROXY_PORT="$2"
+        shift ;;
+    -h) HTTPS_PROXY="$2"
         shift ;;
   esac
   shift
@@ -62,20 +84,21 @@ function set_dns {
 # 写入 docker-compose 的 .env 文件
 function set_env {
   ENV_FILE=".env"
-  domain=$1
-  inter_ip=$2
-  protocol=$3
-
-  echo "DOMAIN=${domain}" > ${ENV_FILE}
-  echo "INTER_IP=${inter_ip}" >> ${ENV_FILE}
-  echo "PROTOCOL=${protocol}" >> ${ENV_FILE}
+  echo "DOMAIN=${DOMAIN}" > ${ENV_FILE}
+  echo "INTER_IP=${INTER_IP}" >> ${ENV_FILE}
+  echo "PROTOCOL=${PROTOCOL}" >> ${ENV_FILE}
+  echo "OPENAI_API_KEY=${OPENAI_API_KEY}" >> ${ENV_FILE}
+  echo "OPENAI_MODEL=${OPENAI_MODEL}" >> ${ENV_FILE}
+  echo "SOCKS_PROXY_HOST=${SOCKS_PROXY_HOST}" >> ${ENV_FILE}
+  echo "SOCKS_PROXY_PORT=${SOCKS_PROXY_PORT}" >> ${ENV_FILE}
+  echo "HTTPS_PROXY=${HTTPS_PROXY}" >> ${ENV_FILE}
 }
 
-# FIXME
-# set_dns $DOMAIN $INTER_IP
-# set_env $DOMAIN $INTER_IP $PROTOCOL
+
+set_dns $DOMAIN $INTER_IP
+set_env
+
 
 docker-compose up -d
-
 docker ps
 echo "Docker is running: ${PROTOCOL}://${DOMAIN}"
